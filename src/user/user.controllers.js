3ab41baf462671f93f55d27857cb6ce0c40f1936
CPerry-92 <90986262+CPerry-92@users.noreply.github.com>
2021-11-16 10:13:37 +0000

    added add user function, tested with TC.

---
+++ src/user/user.controllers.js
@@ -0,0 +1,14 @@
+const User = require("./user.model");
+
+exports.addUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).send({ message: "Success!" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Something went wrong, check server logs" });
+  }
+};
