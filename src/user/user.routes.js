d70ae609e9108206d1bb2d345ba91a70b4b277fc
CPerry-92 <90986262+CPerry-92@users.noreply.github.com>
2021-11-19 14:13:28 +0000

    Udate

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,7 +1,23 @@
 const { Router } = require("express");
-const { addUser } = require("./user.controllers");
+const {
+  addUser,
+  logIn,
+  updateEmail,
+  deleteUser,
+  listUser,
+} = require("./user.controllers");
+const {
+  hashPassword,
+  comparePasswords,
+  tokenAuth,
+} = require("../middleware/index");
 const userRouter = Router();
 
-userRouter.post("/user", addUser);
+userRouter.post("/user", hashPassword, addUser);
+userRouter.post("/login", comparePasswords, logIn);
+userRouter.get("/token", tokenAuth, logIn);
+userRouter.put("/update", updateEmail);
+userRouter.delete("/delete", deleteUser);
+userRouter.get("/list", listUser);
 
 module.exports = userRouter;
